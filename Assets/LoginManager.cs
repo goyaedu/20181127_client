94028f89842fc215eac9d5c55c9faf3aa382b7a2
Junil Ko <goyadb@outlook.com>
2018-11-27 14:32:51 +0900

    로그인 화면 구성

---
+++ Assets/LoginManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour {
+
+    public Image signupPanel;
+    public InputField usernameInputField;
+    public InputField passwordInputField;
+    public InputField confirmPasswordInputField;
+    public InputField nicknameInputField;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // 회원가입 버튼 이벤트
+    public void OnClickSignUpButton()
+    {
+        signupPanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
+    }
+    // 확인 버튼 이벤트
+    public void OnClickConfirmButton()
+    {
+        string password = passwordInputField.text;
+        string confirmPassword = confirmPasswordInputField.text;
+        string username = usernameInputField.text;
+        string nickname = nicknameInputField.text;
+
+        if (string.IsNullOrEmpty(password) || string.IsNullOrEmpty(confirmPassword)
+            || string.IsNullOrEmpty(username) || string.IsNullOrEmpty(nickname))
+        {
+            return;
+        }
+
+        if (password.Equals(confirmPassword))
+        {
+            // TODO: 서버에 회원가입 정보 전송
+
+        }
+
+    }
+    // 취소 버튼 이벤트
+    public void OnClickCancelButton()
+    {
+        signupPanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(900, 0);
+    }
+}
+;
