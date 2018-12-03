48d5e69d3cbba9a8d7a7e8a386f9c4da781bebb5
Junil Ko <goyadb@outlook.com>
2018-12-03 14:11:01 +0900

    Socket.io 초기화 구현

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class NetworkManager : MonoBehaviour
+{
+    SocketIOComponent socket;
+
+	void Start ()
+    {
+        GameObject io = GameObject.Find("SocketIO");
+        socket = io.GetComponent<SocketIOComponent>();
+
+	}
+	
+	void Update () {
+		
+	}
+}
