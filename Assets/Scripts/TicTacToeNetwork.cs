f144aaf855f42598bfab8d283531d8def9ced2ff
Junil Ko <goyadb@outlook.com>
2018-12-05 09:53:00 +0900

    수동 접속 기능 구현

---
+++ Assets/Scripts/TicTacToeNetwork.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+using UnityEngine.UI;
+
+public class TicTacToeNetwork : MonoBehaviour
+{
+    // Start Panel
+    public RectTransform startPanel;
+    public Text startMessageText;
+    public Button connectButton;
+    public Button closeButton;
+
+    // Socket.io
+    SocketIOComponent socket;
+
+    public void Connect()
+    {
+        socket.Connect();
+    }
+
+    public void Close()
+    {
+        socket.Close();
+    }
+
+    private void Start()
+    {
+        GameObject so = GameObject.Find("SocketIO");
+        socket = so.GetComponent<SocketIOComponent>();
+
+        socket.On("joinRoom", JoinRoom);
+        socket.On("createRoom", CreateRoom);
+    }
+
+    void JoinRoom(SocketIOEvent e)
+    {
+        Debug.Log("Join room.");
+    }
+
+    void CreateRoom(SocketIOEvent e)
+    {
+        Debug.Log("Create room.");
+    }
+}
